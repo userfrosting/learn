278829b128e7e128b5adabcd90af6d37ee75d7da
Sarah Baghdadi <sbaghdad@umail.iu.edu>
2017-07-18 00:00:36 -0400

    Hide nav on scroll

--- themes/sitetheme/js/theme.js
+++ themes/sitetheme/js/theme.js
@@ -1,4 +1,18 @@
 $( document ).ready(function() {
    $('a').smoothScroll({offset: -100});
-});
 
+   var $document = $(document),
+       $element = $('#nav-main');
+       
+   $document.scroll(function() {
+       if ($document.scrollTop() <= 200) {
+           $element.stop().css({
+               top: '0px'
+           });
+       } else {
+           $element.stop().css({
+               top: '-200px'
+           });
+       }
+   });
+});
