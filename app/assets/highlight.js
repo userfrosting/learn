91fd8fd070f8de0cafdb809a55e16625e2ae9160
Louis Charette <charette.louis@gmail.com>
2026-02-09 20:24:06 -0500

    Add Vue syntax highlight

--- app/assets/highlight.js
+++ app/assets/highlight.js
@@ -1,5 +1,6 @@
 import CopyButtonPlugin from 'highlightjs-copy'
 import hljs from 'highlight.js'
+import hljsDefineVue from 'highlightjs-vue'
 
 // Initialize Highlight.js with Copy Button plugin
 hljs.addPlugin(
@@ -7,4 +8,5 @@ hljs.addPlugin(
         autohide: false // Always show the copy button
     })
 )
+hljsDefineVue(hljs) // Register Vue language definition
 hljs.highlightAll()
