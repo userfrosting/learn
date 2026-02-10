afbbaa24e4b82e2ad796036101f873e8491eecc4
Louis Charette <charette.louis@gmail.com>
2026-02-09 20:46:22 -0500

    Remove outdated highlightjs-vue dependency from package.json and replace with custom inline solution

--- app/assets/highlight.js
+++ app/assets/highlight.js
@@ -1,6 +1,53 @@
 import CopyButtonPlugin from 'highlightjs-copy'
 import hljs from 'highlight.js'
-import hljsDefineVue from 'highlightjs-vue'
+
+// Define Vue language for Highlight.js
+// Based on: https://github.com/highlightjs/highlightjs-vue
+hljs.registerLanguage('vue', function (hljs) {
+    return {
+        subLanguage: 'xml',
+        contains: [
+            hljs.COMMENT('<!--', '-->', {
+                relevance: 10
+            }),
+            {
+                begin: /^(\s*)(<script>)/gm,
+                end: /^(\s*)(<\/script>)/gm,
+                subLanguage: 'javascript',
+                excludeBegin: true,
+                excludeEnd: true
+            },
+            {
+                begin: /^(\s*)(<script lang=["']ts["']>)/gm,
+                end: /^(\s*)(<\/script>)/gm,
+                subLanguage: 'typescript',
+                excludeBegin: true,
+                excludeEnd: true
+            },
+            {
+                begin: /^(\s*)(<style(\sscoped)?>)/gm,
+                end: /^(\s*)(<\/style>)/gm,
+                subLanguage: 'css',
+                excludeBegin: true,
+                excludeEnd: true
+            },
+            {
+                begin: /^(\s*)(<style lang=["'](scss|sass)["'](\sscoped)?>)/gm,
+                end: /^(\s*)(<\/style>)/gm,
+                subLanguage: 'scss',
+                excludeBegin: true,
+                excludeEnd: true
+            },
+            {
+                begin: /^(\s*)(<style lang=["']stylus["'](\sscoped)?>)/gm,
+                end: /^(\s*)(<\/style>)/gm,
+                subLanguage: 'stylus',
+                excludeBegin: true,
+                excludeEnd: true
+            }
+        ]
+    }
+})
 
 // Initialize Highlight.js with Copy Button plugin
 hljs.addPlugin(
@@ -8,5 +55,4 @@ hljs.addPlugin(
         autohide: false // Always show the copy button
     })
 )
-hljsDefineVue(hljs) // Register Vue language definition
 hljs.highlightAll()
